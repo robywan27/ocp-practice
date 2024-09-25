36e51f0df54c579034864ca65ba485b9102ceced
robywan <roberto.napoli@kpn.com>
2024-09-25 17:20:53 +0200

    Add switch statement and expression

---
+++ src/chapter3/SwitchExpression.java
@@ -0,0 +1,95 @@
+package chapter3;
+
+/*Switch expression
+* Compact form of switch statement. It can return a value, which can be assigned to a variable.
+* All case and default branches must return a type compatible with the assignment.
+* Two types of branches supported:
+*   - an expression
+*   - a block
+* */
+public class SwitchExpression {
+    public void printDayOfTheWeek(int day) {
+        var result = switch (day) {
+            case 0 -> "Saunday"; // arrow operator -> instead of colon :, case branch ain't a lambda expression though!
+            case 1 -> { yield "Monday"; } // if case block, yield is required when returning a value
+            case 2 -> "Tuesday"; // semicolon required
+            case 3 -> "Wednesday"; // no break statement required
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid day"; // default is required, if all case values aren't handled
+        }; // semicolon required here too, because of assignment
+        System.out.println(result);
+    }
+
+    public void printSeason(int month) {
+        switch (month) {
+            case 1, 2, 3 -> System.out.println("Winter");
+            case 4, 5, 6 -> System.out.println("Spring");
+            case 7, 8, 9 -> System.out.println("Summer");
+            case 10, 11, 12 -> System.out.println("Fall");
+        } // in this case semicolon is not required because no value is returned
+    }
+
+    public static void main(String[] args) {
+        var bear = 1;
+        var result = switch (bear) {
+//            case 3 -> "Grizzly" // semicolon required
+            case 3 -> "Grizzly";
+            default -> "Panda";
+//        } // semicolon required
+        };
+
+        /*Rules for switch expressions
+         * 1. all the branches that don't throw an exception must return a consistent data type, if the switch
+         * expression returns a value
+         * 2. if switch expr returns a value, then every branch that isn't an expression (case blocks) must yield a value
+         * 3. a default branch is mandatory unless all cases are covered or no value is returned
+         * */
+
+        // Rule 1.
+        int measurement = 10;
+        int size = switch (measurement) {
+            case 5 -> 1;
+            case 10 -> (short) 2;
+            default -> 5;
+//            case 20 -> "ciao";
+//            case 40 -> 4L;
+//            case 33 -> null;
+        };
+
+        // Rule 2.
+        int fish = 5;
+        int length = 12;
+        var name = switch (fish) {
+            case 1 -> "Goldfish";
+            case 2 -> { yield "Trout"; }
+            case 3 -> {
+                if (length > 10) yield "Blobfish";
+                else yield "Green"; // else is mandatory, all branches must yield a value within the case block
+            }//; no semicolons at the end of blocks
+            default -> "Swordfish";
+        };
+
+        // Rule 3.
+        int canis = 1;
+        String type = switch (canis) {
+            case 1 -> "dog";
+            case 2 -> "wolf";
+            case 3 -> "coyote";
+            default -> "cat"; // required! or won't compile
+        };
+
+        enum Season { WINTER, SPRING, SUMMER, FALL}
+        Season season = Season.WINTER;
+        var s = switch (season) {
+            case WINTER -> "Winter";
+            case SPRING -> "Spring";
+            case SUMMER -> "Summer";
+            case FALL -> "Fall";
+            // default is not needed here because all enum values have been covered
+        };
+        // Tip: add default always, also with enums, because in the future one can add a new value and code won't
+        // compile anymore
+    }
+}
--- src/chapter3/SwitchStatement.java
+++ src/chapter3/SwitchStatement.java
@@ -9,7 +9,7 @@ package chapter3;
 * */
 public class SwitchStatement {
     public static void printDayOfTheWeek(int day) {
-        switch(day) {
+        switch (day) {
             case 0:
                 System.out.println("Sunday");
                 break; // optional
@@ -39,8 +39,10 @@ public class SwitchStatement {
 
     public static void pickAnimal(int animal) {
         switch (animal) {
-            case 0, 1: System.out.println("Lion"); // comma-separated case values
-            case 2: System.out.println("Giraffe"); // this is always called because there is no break in the previous
+            case 0, 1:
+                System.out.println("Lion"); // comma-separated case values
+            case 2:
+                System.out.println("Giraffe"); // this is always called because there is no break in the previous
                 // case statement
         }
     }
@@ -51,5 +53,63 @@ public class SwitchStatement {
         pickAnimal(1); // prints Lion and Giraffe
         pickAnimal(2); // Giraffe
         pickAnimal(3); // prints nothing
+
+        /*Valid data types
+        * int, Integer
+        * byte, Byte
+        * short, Short
+        * char, Character
+        * String
+        * enum values
+        * var, if the type resolves to any one of the previous mentioned types
+        * */
+        short sh = 12;
+        switch (sh) {};
+        char ch = 'q';
+        switch (ch) {};
+        switch ("str") {};
+        var variable = "A variable var of valid type";
+        switch (variable) {};
+
+        enum DayOfTheWeek { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY };
+        DayOfTheWeek day = DayOfTheWeek.WEDNESDAY;
+        switch (day) {
+            case MONDAY: System.out.println("Monday"); break;
+            case TUESDAY: System.out.println("Tuesday"); break;
+            case WEDNESDAY: System.out.println("Wednesday"); break;
+            case THURSDAY: System.out.println("Thursday"); break;
+            case FRIDAY: System.out.println("Friday"); break;
+            case SATURDAY: System.out.println("Saturday"); break;
+            case SUNDAY: System.out.println("Sunday"); break;
+        }
+
+        // invalid
+        long longVal = 10L;
+//        switch (longVal) {} // long values are not valid
+        float flNum = 3.4f;
+//        switch (flNum) {};
+        boolean bool = true;
+//        switch (bool) {};
+    }
+
+    /*Valid case values
+     * Must be compile-time constant values of the same data type as the switch value.
+     * - literals
+     * - enum constants
+     * - final constant variables of the same data type; must be initialized with a literal value at declaration
+     * */
+    final int getCookies() { return 4; }
+    void feedAnimals() {
+        final int bananas = 1;
+        int apples = 2;
+        int numberOfAnimals = 3;
+        final int cookies = getCookies();
+        switch (numberOfAnimals) {
+            case bananas:
+//            case apples:  // not constant
+//            case getCookies(): // methods are evaluated at runtime
+//            case cookies: // same, even with final variable
+            case 1 + 2: // expressions are allowed
+        }
     }
 }
